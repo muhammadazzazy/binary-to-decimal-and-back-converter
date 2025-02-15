1b46041abcb3761a92ef2d08d0b311c0a74e13b6
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-15 16:34:31 +0200

    improved exception handling, added type annotations, and added functionality for accepting the base as an English word

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sys import exit
+from word2number import w2n
 
 
 def is_binary(binary_string: str) -> bool:
@@ -12,7 +13,7 @@ def is_binary(binary_string: str) -> bool:
 
 
 def bin2dec(binary_string: str) -> int:
-    dec_val = 0
+    dec_val: int = 0
     for i in range(len(binary_string)):
         if binary_string[i] == '1':
             dec_val += pow(2, len(binary_string)-1-i)
@@ -21,7 +22,7 @@ def bin2dec(binary_string: str) -> int:
 
 
 def dec2bin(decimal_value: int) -> str:
-    binary_string = ''
+    binary_string: str = ''
     while decimal_value > 0:
         binary_string += str(decimal_value % 2)
         decimal_value = decimal_value // 2
@@ -29,17 +30,19 @@ def dec2bin(decimal_value: int) -> str:
 
 
 def main() -> None:
-    print('This program converts from binary to decimal and vice versa!')
+    print('Welcome to The Base-2️⃣-to-Base-🔟-and-Back Converter!')
     base_dict = {'binary': 2, 'decimal': 10}
-
+    exit_message: str = 'Exiting program...'
     while True:
         try:
-            user_input = input('Enter the base to convert to: ')
+            user_input: str = input('Enter the base to convert to: ')
             if user_input == 'exit':
-                print('Thanks for trying my program!')
+                print(exit_message)
                 exit()
-
-            base = int(user_input)
+            if user_input.isnumeric():
+                base: int = int(user_input)
+            else:
+                base: int = w2n.word_to_num(user_input)
 
             if base not in base_dict.values():
                 print('Base can be either 2 or 10...')
@@ -48,24 +51,24 @@ def main() -> None:
             user_input = input('Enter the value to convert: ')
 
             if base == base_dict['binary']:
-                dec_val = int(user_input)
-                bin_equiv = dec2bin(dec_val)
+                dec_val: int = int(user_input)
+                bin_equiv: str = dec2bin(dec_val)
                 print(f'The binary equivalent of {user_input} is {bin_equiv}.')
-            elif base == base_dict['decimal']:
+            else:
                 if is_binary(user_input):
-                    bin_val = user_input
-                    dec_equiv = bin2dec(bin_val)
+                    bin_val: str = user_input
+                    dec_equiv: int = bin2dec(bin_val)
                     print(
                         f'The decimal equivalent of {user_input} is {dec_equiv}.')
                 else:
-                    print('Invalid binary string...')
+                    print('Please enter a valid binary string...')
                     continue
-            else:
-                print('Invalid base...')
+
         except ValueError:
-            print('Invalid input value...')
+            print('Please enter valid input...')
+
         except KeyboardInterrupt:
-            print('Exiting...')
+            print(exit_message)
             exit()
 
 
